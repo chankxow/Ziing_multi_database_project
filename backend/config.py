1243fb522e2e8318144e9bcba3ff86ed6857481b
chankxow <kittipoomzx555@gmail.com>
2026-02-26 10:09:52 +0700

    add page

--- backend/config.py
+++ backend/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 # MySQL Configuration
 MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3307))
+MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
 MYSQL_USER = os.getenv("MYSQL_USER", "root")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "yourpass")
 MYSQL_DB = os.getenv("MYSQL_DB", "CarCustomShop")
