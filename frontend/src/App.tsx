5870856d16449343bace9534eade79a251919612
Chankxow <kittipoomzx555@gmail.com>
2026-02-28 16:36:13 +0700

    new fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/register";
-//import Register from "./Register";
-//<Route path="/register" element={<Register />} />
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        {/* แก้ props ที่ผิด */}
+        {/* Catch all - ต้องอยู่สุดท้าย */}
+        <Route path="*" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
