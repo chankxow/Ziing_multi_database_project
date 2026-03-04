18501db011f2bb74b1bbfedabbdb30da52a75e04
Ampser <40409@chs.ac.th>
2026-03-04 14:27:21 +0700

    now you can innit admin by run addmin_innit.py

---
+++ backend/admin_innit.py
@@ -0,0 +1,19 @@
+import bcrypt
+import pymysql
+
+conn = pymysql.connect(host="localhost", user="root", password="YOUR_MYSQL_PASSWORD", database="CarCustomShop")
+cur = conn.cursor()
+
+username  = "admin_"
+password  = "1234"
+firstName = "Ton"
+lastName  = "Manager"
+
+hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+cur.execute(
+    "INSERT INTO User (Username, PasswordHash, FirstName, LastName, RoleID) VALUES (%s,%s,%s,%s,1)",
+    (username, hashed, firstName, lastName)
+)
+conn.commit()
+print("สร้าง Admin สำเร็จ")
+conn.close()
